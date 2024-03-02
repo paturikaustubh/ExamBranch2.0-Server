43a42592fc8df14e05001ac0ce41635bea9b736b
paturikaustubh <plutofeb115@gmail.com>
2024-03-02 10:13:18 +0530

    selectedSubjects

--- src/reval/routes.ts
+++ src/reval/routes.ts
@@ -1,18 +1,14 @@
 import { Router } from "express";
 import { printReval, registerReval, revalSearch } from "./controller";
 
-
 const router: Router = Router();
 
 // Registering all the Revaluation module routes
 
 router.get("/search", revalSearch);
 
-router.post("/print/:rollNum", printReval);
+router.post("/print/:rollNo", printReval);
 
-router.post("/register/:rollNum", registerReval);
-
-
-
+router.post("/register/:rollNo", registerReval);
 
 export default router;
